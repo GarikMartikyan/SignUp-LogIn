2de3be589e99f6c79bf8ac54ee833f695f1f30d7
Garik <garikmartikyan03@gmail.com>
2024-01-21 01:47:57 +0400

    Added Simple Validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,35 @@
-import Input from "../Input/Input.jsx";
-import styled from "styled-components";
-import Button from "../Button/Button.jsx";
-import classes from './Login.module.css'
-import PassInput from "../Input/PassInput.jsx";
+import Input from '../Input/Input.jsx';
+import styled from 'styled-components';
+import Button from '../Button/Button.jsx';
+import classes from './Login.module.css';
+import PassInput from '../Input/PassInput.jsx';
 
 const A = styled.a`
-  text-decoration: none;
-  color: #300b9a;
-`
+	text-decoration: none;
+	color: #300b9a;
+`;
 
 export default function Login() {
+	function isValid(e) {
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+		const valid = email.trim().length || password.trim().length;
 
-    return (
-        <div>
-            <form name="login" action="#" autoComplete="on">
-                <Input name="email" type="email" iconName="email" placeholder="Email"/>
-                <PassInput/>
-                <p>Lost password? <A href="#">Click here!</A></p>
-                <div className={classes.centeredDiv}>
-                    <Button children="Enter" isActive={true} type="submit"/>
-                </div>
-            </form>
-        </div>
-    )
+		if (!valid) e.preventDefault();
+	}
+
+	return (
+		<div>
+			<form name='login' action='#' autoComplete='on' onSubmit={isValid}>
+				<Input name='email' type='email' iconName='email' placeholder='Email' />
+				<PassInput />
+				<p>
+					Lost password? <A href='#'>Click here!</A>
+				</p>
+				<div className={classes.centeredDiv}>
+					<Button children='Enter' isActive={true} type='submit' />
+				</div>
+			</form>
+		</div>
+	);
 }
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,20 +1,28 @@
-import Input from "../Input/Input.jsx";
-import classes from './SignUp.module.css'
-import Button from "../Button/Button.jsx";
-import PassInput from "../Input/PassInput.jsx";
+import Input from '../Input/Input.jsx';
+import classes from './SignUp.module.css';
+import Button from '../Button/Button.jsx';
+import PassInput from '../Input/PassInput.jsx';
 
 export default function SignUp() {
+	function isValid(e) {
+		const name = e.target.name.value;
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+		const valid =
+			name.trim().length || email.trim().length || password.trim().length;
+		if (!valid) e.preventDefault();
+	}
 
-    return (
-        <div>
-            <form name="signup" action="#" autoComplete="on">
-                <Input name="name" iconName="person" placeholder="Name"/>
-                <Input name="email" type="email" iconName="mail" placeholder="Email"/>
-                <PassInput/>
-                <div className={classes.centeredDiv}>
-                    <Button type="submit" isActive={true} children="Register"/>
-                </div>
-            </form>
-        </div>
-    )
+	return (
+		<div>
+			<form name='signup' action='#' autoComplete='on' onSubmit={isValid}>
+				<Input name='name' iconName='person' placeholder='Name' />
+				<Input name='email' type='email' iconName='mail' placeholder='Email' />
+				<PassInput />
+				<div className={classes.centeredDiv}>
+					<Button type='submit' isActive={true} children='Register' />
+				</div>
+			</form>
+		</div>
+	);
 }
